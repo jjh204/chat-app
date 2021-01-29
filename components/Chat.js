617629e1ac49f8401ad98005dd1b46b6d0c98fe5
jjh204 <59932565+jjh204@users.noreply.github.com>
2021-01-29 14:40:24 +0100

    adding comments

--- components/Chat.js
+++ components/Chat.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default class Chat extends React.Component {
   render() {
     let name = this.props.route.params.name;
     let color = this.props.route.params.color;
+
+    // I believe this is currently causing a warning to display 'cannot update component from inside function body of a different component'
     this.props.navigation.setOptions({ title: name });
 
     return (
