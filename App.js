e6c374386655fea9d80786dab23d426c4209a07e
jjh204 <59932565+jjh204@users.noreply.github.com>
2021-01-29 13:40:06 +0100

    adding comments

--- App.js
+++ App.js
@@ -5,9 +5,11 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// allowing navigation between different app screens
 const Stack = createStackNavigator();
 
 export default function App() {
+  // setting up the homepage first then connecting the additional screens.
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Start">
