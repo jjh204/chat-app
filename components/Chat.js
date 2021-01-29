a03084f2f84a0cd5aeaa122c15b83fc5177926e7
jjh204 <59932565+jjh204@users.noreply.github.com>
2021-01-29 13:40:37 +0100

    updating this.state.color

--- components/Chat.js
+++ components/Chat.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default class Chat extends React.Component {
   render() {
     let name = this.props.route.params.name;
+    let color = this.props.route.params.color;
     this.props.navigation.setOptions({ title: name });
 
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: color }}>
         <Text>Lets Start Chatting!</Text>
       </View>
     )
